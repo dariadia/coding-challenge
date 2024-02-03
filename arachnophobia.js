f91edec3f2d9d5a26da6e8911dad0ff6d220c30b
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-03 16:05:46 +0100

    Create arachnophobia.js

---
+++ arachnophobia.js
@@ -0,0 +1,20 @@
+/* From https://www.codewars.com/kata/5bc73331797b005d18000255
+
+There is no single treatment that works for every phobia, but some people cure it by being gradually exposed to the phobic situation or object. In this kata we will try curing arachnophobia by drawing primitive spiders.
+
+Our spiders will have legs, body, eyes and a mouth. Here are some examples:
+
+/\((OOwOO))/\
+
+/╲(((0000w0000)))╱\
+
+^(oWo)^
+You will be given four values:
+
+leg size where each value stands for its own leg type: 1 for "^ ^", 2 for "/\ /\", 3 for "/╲ ╱\", 4 for "╱╲ ╱╲"
+body size where each value stands for its own body type: 1 for "( )", 2 for "(( ))", 3 for "((( )))"
+mouth representing the spider's mouth
+eye representing the spider's eye
+Note: the eyes are symmetric, and their total amount is 2 to the power of body size.
+
+*/
