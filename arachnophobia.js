ac50901f7093bcf134f5409adb3f8b9a7b3072d7
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-03 16:36:48 +0100

    Update arachnophobia.js

--- arachnophobia.js
+++ arachnophobia.js
@@ -18,3 +18,17 @@ eye representing the spider's eye
 Note: the eyes are symmetric, and their total amount is 2 to the power of body size.
 
 */
+
+const arachnophobia = (legSize, bodySize, mouth, eye) => {
+    const LEG_SIZES = ["", "^ ^", "/| |/", "\|| ||/", "\| |/"]
+    const BODY_SIZES = ["", "( )", "(( ))", "((( )))"]
+    const EYE_COUNT = Math.pow(2, bodySize)
+    return LEG_SIZES[legSize].split(" ")[0] 
+      + BODY_SIZES[bodySize].split(" ")[0] 
+      + Array.from({ length: EYE_COUNT / 2 }, () => eye).join("") 
+      + mouth 
+      + BODY_SIZES[bodySize].split(" ")[1] 
+      + LEG_SIZES[legSize].split(" ")[1]
+}
+
+console.log(arachnophobia(1,2,"w","o"))
