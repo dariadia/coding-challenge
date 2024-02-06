9314b1fa95a972456d1dd82219199cdd0942c7de
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-06 12:47:15 +0100

    Create password-security-ii.js

---
+++ password-security-ii.js
@@ -0,0 +1,12 @@
+/* From https://jscodebox.com/challenge/password-security-ii
+
+Given are two strings password and password_repeat. Check if the password is secure by the following critera:
+- both passwords must match
+- password must be at least 8 chars
+- contains at least a number
+- contains at lease an uppercase letter
+- contains at least a lowercase letter
+- contains at least one of these special chars (&$%§-_)
+Return true only if all criteria are met, otherwise return false.
+*/
+
