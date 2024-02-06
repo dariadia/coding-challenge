9b4537edb1e3aca78816397bf435e7827df7a423
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-06 12:44:42 +0100

    Update ghappy.js

--- ghappy.js
+++ ghappy.js
@@ -1,3 +1,9 @@
 // From https://jscodebox.com/challenge/ghappy
 // A small 'g' is 'happy', but only if a small 'g' follows before or after it. Return true if all g's are happy.
 
+function gHappy(string) {
+  const G = "g"
+  const gPlace = string.indexOf(G)
+  return string[gPlace - 1] === G 
+    || string[gPlace + 1] === G 
+}
