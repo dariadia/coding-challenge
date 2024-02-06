7b115aedc72af93adb87e6a8d3038e89cb9303a5
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-06 12:46:29 +0100

    Create first-hello.js

---
+++ first-hello.js
@@ -0,0 +1,6 @@
+// From https://jscodebox.com/challenge/first-hello
+// A string sentence is given. Return true if the block starts with "Hello".
+
+function firstHello(sentence) {
+  return sentence.indexOf("Hello") === 0
+}
